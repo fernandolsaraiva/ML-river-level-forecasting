7684d7f765dcdc62695c43d8a557b022e1fe5fca
Fernando Saraiva <fernandofilhols7@gmail.com>
2023-05-22 16:06:51 -0300

    mudei a lógica: agora eu pego simplesmente a última hora disponível, aviso qual é e vou fornecer para dar a previsão para as próximas 2 horas. Se não houver dados nas últimas 24 horas, vou dizer que não há dados disponíveis

--- integrador_nivel.py
+++ integrador_nivel.py
@@ -12,24 +12,35 @@ def getJson(url):
     return json_value
 
 def getDate():
-    date = datetime.now() # current date and time
+    date = datetime.utcnow() # current date and time
     return date
 
 def getNivel(date):
     year = date.strftime("%Y")
     month = date.strftime("%m")
     day = date.strftime("%d")
-    time = date.strftime("%H:%M:%S")
-    date_time = date.strftime("%m/%d/%Y, %H:%M:%S")	
-    
+    date_yesterday = date-timedelta(days=1)
+    year_yesterday = date_yesterday.strftime("%Y")
+    month_yesterday = date_yesterday.strftime("%m")
+    day_yesterday = date_yesterday.strftime("%d")
+
     #Posto 33767 é a estação 413
-    url = f"http://sibh.daee.sp.gov.br/grafico_nivel_novo.csv?postos[]=33767&periodo={day}/{month}/{year}%20-%20{day}/{month}/{year}&tipo_dados=coleta&formato=1"
+    url = f"http://sibh.daee.sp.gov.br/grafico_nivel_novo.csv?postos[]=33767&periodo={day_yesterday}/{month_yesterday}/{year_yesterday}%20-%20{day}/{month}/{year}&tipo_dados=coleta&formato=1"
     json_value = getJson(url)
     with open(r"/Users/fernandosaraiva/Desktop/Mestrado/Engenharia_de_Software/Nivel.csv","w") as file:
         file.write(json_value + "\n")
-    return json_value
+    df = pd.read_csv(r"/Users/fernandosaraiva/Desktop/Mestrado/Engenharia_de_Software/Nivel.csv")
+    if df.empty:
+        print('Não há dados disponíveis')
+        last_level = last_time = json_value = None
+        return last_level,last_time,json_value
+    last_level = df['valor_leitura_flu'].iloc[-1]
+    last_time = df['intervalo'].iloc[-1]
+    print('Last available river level: ',last_level)
+    print('Last available time: ',last_time)
+    return last_level,last_time,json_value
 
 if __name__ == '__main__':
     date = getDate()    
-    nivel = getNivel(date)
+    nivel,time,json_value = getNivel(date)
     print('Concluído')
