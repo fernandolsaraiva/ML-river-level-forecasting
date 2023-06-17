ace75aa37a81d269a91a8003cff29495d341608b
Hideki Inoue <hideki.i@protonmail.com>
2023-06-17 17:53:12 -0300

    feat: map pins, pin popup button, chart centered and linked button.

--- frontend/maps.js
+++ frontend/maps.js
@@ -1,6 +1,15 @@
-var map = L.map('map').setView([-23.1914, -45.8791], 13);
+var map = L.map('map').setView([-23.589799, -46.588237], 12);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(map);
+}).addTo(map);
+
+let popupContent1 = "Rio Tamanduatei - 143<br \\><button id='map_button' onclick=\"fetchAndPlotData_station('143')\">Ver Graficos</button>"
+let popupContent2 = "Rio Tamanduatei - 1000958<br \\><button id='map_button' onclick=\"fetchAndPlotData_station('1000958')\">Ver Graficos</button>"
+let popupContent3 = "Rio Tamanduatei - 413<br \\><button id='map_button' onclick=\"fetchAndPlotData_station(413)\">Ver Graficos</button>"
+
+L.marker([-23.61085, -46.544628]).addTo(map).bindPopup(popupContent1);
+L.marker([-23.589799, -46.588237]).addTo(map).bindPopup(popupContent2);
+L.marker([-23.541539, -46.628423]).addTo(map).bindPopup(popupContent3);
+
--- frontend/script.js
+++ frontend/script.js
@@ -20,7 +20,73 @@ plotButton.addEventListener("click", () => {
   const apiUrl = `http://140.238.182.60:8000/river?station=${selectedEndPoint}`;
 
 
-function fetchAndPlotData() {
+  function fetchAndPlotData() {
+
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        const riverData = data;
+
+        const chartData = {
+          labels: riverData.data.map(([time, waterLevel]) => time),
+          datasets: [{
+            label: 'Nível da Água',
+            data: riverData.data.map(([time, waterLevel]) => waterLevel),
+            backgroundColor: (ctx) => {
+              const index = ctx.dataIndex;
+              const length = ctx.dataset.data.length;
+              return index < length / 2 ? 'rgba(0, 0, 255, 1)' : 'rgba(255, 0, 0, 1)';
+            },
+            borderWidth: 10
+          }]
+        };
+
+        if (chart) {
+          chart.destroy();
+        }
+
+        const ctx = document.getElementById('myChart').getContext('2d');
+        chart = new Chart(ctx, {
+          type: 'line',
+          data: chartData,
+          options: {
+            title: {
+              display: true,
+              text: riverData.station
+            },
+            options: {
+              scales: {
+                yAxes: [{
+                  ticks: {
+                    beginAtZero: true
+                  }
+                }]
+              }
+            }
+          }
+        });
+
+        document.getElementById('chart-title').textContent = `Nível do rio na estação ${riverData.station}`;
+      })
+      .catch(error => {
+        console.error('There was a problem with the API call:', error);
+      });
+  }
+
+  const plotButton = document.getElementById("plot-button");
+  plotButton.addEventListener("click", fetchAndPlotData);
+
+
+  fetchAndPlotData();
+})
+
+function fetchAndPlotData_station(station) {
+  const apiUrl = `http://140.238.182.60:8000/river?station=${station}`;
 
   fetch(apiUrl)
     .then(response => {
@@ -77,11 +143,4 @@ function fetchAndPlotData() {
       console.error('There was a problem with the API call:', error);
     });
 }
-
-const plotButton = document.getElementById("plot-button");
-plotButton.addEventListener("click", fetchAndPlotData);
-
-
-fetchAndPlotData();
-})
 
