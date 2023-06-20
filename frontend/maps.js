eeb7ccae9beb29b2a9eca67f3caca13984da58e7
Anderson Sales <andersonsales.mg@gmail.com>
2023-06-20 16:29:42 -0300

    creates buttons to download csv and AI data, adjusts the timezone to BRT, and edits the subtitle of the graph to explain the displayed data

--- frontend/maps.js
+++ frontend/maps.js
@@ -5,9 +5,9 @@ L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
 
-let popupContent1 = "Rio Tamanduatei - 143<br \\><button id='map_button' onclick=\"fetchAndPlotData_station('143')\">Ver Graficos</button>"
-let popupContent2 = "Rio Tamanduatei - 1000958<br \\><button id='map_button' onclick=\"fetchAndPlotData_station('1000958')\">Ver Graficos</button>"
-let popupContent3 = "Rio Tamanduatei - 413<br \\><button id='map_button' onclick=\"fetchAndPlotData_station(413)\">Ver Graficos</button>"
+let popupContent1 = "Rio Tamanduatei - 143<br \\><button id='map_button' onclick=\"fetchAndPlotData_station('143')\">Gerar Gráfico</button>"
+let popupContent2 = "Rio Tamanduatei - 1000958<br \\><button id='map_button' onclick=\"fetchAndPlotData_station('1000958')\">Gerar Gráfico</button>"
+let popupContent3 = "Rio Tamanduatei - 413<br \\><button id='map_button' onclick=\"fetchAndPlotData_station(413)\">Gerar Gráfico</button>"
 
 L.marker([-23.61085, -46.544628]).addTo(map).bindPopup(popupContent1);
 L.marker([-23.589799, -46.588237]).addTo(map).bindPopup(popupContent2);
--- frontend/script.js
+++ frontend/script.js
@@ -1,89 +1,7 @@
-const endPointList = [
-  { name: 'Estação 1', value: 1000958 },
-  { name: 'Estação 2', value: 1000958 },
-  { name: 'Estação 3', value: 1000958 }
-];
-let chart = null
-
-const riverSelect = document.getElementById("river-select");
-endPointList.forEach(endPoint => {
-  const option = document.createElement("option");
-  option.value = endPoint.value;
-  option.text = endPoint.name;
-  riverSelect.appendChild(option);
-});
-
-const plotButton = document.getElementById("plot-button");
-plotButton.addEventListener("click", () => {
-  const selectedEndPoinIndex = riverSelect.selectedIndex;
-  const selectedEndPoint = endPointList[selectedEndPoinIndex].value;
-  const apiUrl = `http://140.238.182.60:8000/river?station=${selectedEndPoint}`;
-
-
-  function fetchAndPlotData() {
-
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        const riverData = data;
-
-        const chartData = {
-          labels: riverData.data.map(([time, waterLevel]) => time),
-          datasets: [{
-            label: 'Nível da Água',
-            data: riverData.data.map(([time, waterLevel]) => waterLevel),
-            backgroundColor: (ctx) => {
-              const index = ctx.dataIndex;
-              const length = ctx.dataset.data.length;
-              return index < length / 2 ? 'rgba(0, 0, 255, 1)' : 'rgba(255, 0, 0, 1)';
-            },
-            borderWidth: 10
-          }]
-        };
-
-        if (chart) {
-          chart.destroy();
-        }
-
-        const ctx = document.getElementById('myChart').getContext('2d');
-        chart = new Chart(ctx, {
-          type: 'line',
-          data: chartData,
-          options: {
-            title: {
-              display: true,
-              text: riverData.station
-            },
-            options: {
-              scales: {
-                yAxes: [{
-                  ticks: {
-                    beginAtZero: true
-                  }
-                }]
-              }
-            }
-          }
-        });
-
-        document.getElementById('chart-title').textContent = `Nível do rio na estação ${riverData.station}`;
-      })
-      .catch(error => {
-        console.error('There was a problem with the API call:', error);
-      });
-  }
-
-  const plotButton = document.getElementById("plot-button");
-  plotButton.addEventListener("click", fetchAndPlotData);
-
-
-  fetchAndPlotData();
-})
+let chart = null;
+let jsonData = null;
+let downloadDataButton = null;
+let downloadAIParametersButton = null;
 
 function fetchAndPlotData_station(station) {
   const apiUrl = `http://140.238.182.60:8000/river?station=${station}`;
@@ -96,12 +14,13 @@ function fetchAndPlotData_station(station) {
       return response.json();
     })
     .then(data => {
+      jsonData = data;
       const riverData = data;
 
       const chartData = {
-        labels: riverData.data.map(([time, waterLevel]) => time),
+        labels: riverData.data.map(([time, waterLevel]) => convertUTCToBRT(time)),
         datasets: [{
-          label: 'Nível da Água',
+          label: 'Nível da águal em metros acima do nível do mar. Dados reais em azul x dados previstos para as próximas duas horas em vermelho.',
           data: riverData.data.map(([time, waterLevel]) => waterLevel),
           backgroundColor: (ctx) => {
             const index = ctx.dataIndex;
@@ -112,20 +31,16 @@ function fetchAndPlotData_station(station) {
         }]
       };
 
-      if (chart) {
-        chart.destroy();
-      }
-
-      const ctx = document.getElementById('myChart').getContext('2d');
-      chart = new Chart(ctx, {
-        type: 'line',
-        data: chartData,
-        options: {
-          title: {
-            display: true,
-            text: riverData.station
-          },
+      if (!chart) {
+        const ctx = document.getElementById('myChart').getContext('2d');
+        chart = new Chart(ctx, {
+          type: 'line',
+          data: chartData,
           options: {
+            title: {
+              display: true,
+              text: riverData.station
+            },
             scales: {
               yAxes: [{
                 ticks: {
@@ -134,13 +49,73 @@ function fetchAndPlotData_station(station) {
               }]
             }
           }
-        }
-      });
+        });
+      } else {
+        chart.data = chartData;
+        chart.update();
+      }
 
       document.getElementById('chart-title').textContent = `Nível do rio na estação ${riverData.station}`;
+
+      if (downloadDataButton) {
+        downloadDataButton.removeEventListener('click', downloadData);
+      }
+      downloadDataButton = document.getElementById('download-data-button');
+      downloadDataButton.addEventListener('click', downloadData);
+
+      if (downloadAIParametersButton) {
+        downloadAIParametersButton.removeEventListener('click', downloadData);
+      }
+      downloadAIParametersButton = document.getElementById('download-parameters-button');
+      downloadAIParametersButton.addEventListener('click', downloadData);
+
     })
     .catch(error => {
       console.error('There was a problem with the API call:', error);
     });
 }
 
+function convertUTCToBRT(time) {
+  const [hours, minutes] = time.split(':');
+  const utcHours = parseInt(hours, 10);
+  const utcMinutes = parseInt(minutes, 10);
+  const brtHours = (utcHours - 3 + 24) % 24;
+  const brtMinutes = utcMinutes;
+  const brtTime = `${brtHours.toString().padStart(2, '0')}:${brtMinutes.toString().padStart(2, '0')}h`;
+  return brtTime;
+}
+
+function downloadData() {
+  downloadCSV(jsonData);
+}
+
+function downloadCSV(jsonData) {
+  const csvContent = convertToCSV(jsonData);
+  const encodedURI = encodeURI(`data:text/csv;charset=utf-8,${csvContent}`);
+  const link = document.createElement('a');
+  link.setAttribute('href', encodedURI);
+  link.setAttribute('download', `Station_${jsonData.station}_data.csv`);
+  document.body.appendChild(link);
+  link.click();
+  document.body.removeChild(link);
+}
+
+function convertToCSV(jsonData) {
+  let csvContent = '';
+
+  const headers = Object.keys(jsonData);
+  csvContent += headers.join(',') + '\n';
+
+  for (const header of headers) {
+    const value = jsonData[header];
+
+    if (Array.isArray(value)) {
+      const row = value.map(item => JSON.stringify(item)).join(',');
+      csvContent += row + '\n';
+    } else {
+      const row = JSON.stringify(value);
+      csvContent += row + '\n';
+    }
+  }
+  return csvContent;
+}
